9d51100b3e0a38b395b943e3df25efa10009dcb5
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-03-27 19:56:42 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="max-container">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="font-rubik text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+}
