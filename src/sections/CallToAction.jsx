3c617195df679d39e7dcd37cbfd9ec50b612de29
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-04-01 17:11:54 +0200

    add email validation

--- src/sections/CallToAction.jsx
+++ src/sections/CallToAction.jsx
@@ -1,7 +1,34 @@
 import Button from "../components/Button";
 import errorIcon from "../Assets/images/icon-error.svg";
+import { useState } from "react";
 
 const CallToAction = () => {
+  const [input, setInput] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  console.log("email:" + input);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setInput(value);
+    setIsValid(validateEmail(input));
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(input)) {
+      setIsValid(validateEmail(input));
+      return;
+    }
+    alert("Thank you for Joining Our Newsletter");
+    setInput("");
+  };
+
   return (
     <section className="flex flex-col items-center   bg-soft_Blue text-slate-50">
       <div className="text-center max-container max-w-[442px] my-14 md:my-[75px]">
@@ -11,23 +38,39 @@ const CallToAction = () => {
         <h3 className="text-2xl md:text-3xl font-medium leading-7 md:leading-10 tracking-[-0.08px] md:tracking-[-0.1px]">
           Stay up-to-date with what we’re doing
         </h3>
-        <form className="md:mt-8 md:flex md:items-start md:gap-4">
+        <form
+          onSubmit={handleSubmit}
+          className="md:mt-8 md:flex md:items-start md:gap-4"
+        >
           <div className="flex-grow  mt-8 md:mt-0  ">
             <div
+              style={{
+                border: isValid ? "" : "2px solid #FA5959",
+              }}
               className=" flex items-center justify-between 
-            p-3  bg-white border rounded  border-soft_Red border-2 border-hidden"
+            px-3 py-2  bg-white rounded "
             >
               <input
+                name={input}
+                value={input}
+                onChange={handleChange}
                 className=" w-full text-sm leading-7 tracking-[0.25px]
                  text-dark_Blue outline-none placeholder:text-base placeholder:leading-7 placeholder:tracking-[0.15px]"
                 type="text"
                 placeholder="Enter your email address"
               />
-              <img className="hidden" src={errorIcon} alt="" />
+              <img
+                style={{ display: isValid ? "none" : "block" }}
+                className="hidden"
+                src={errorIcon}
+                alt=""
+              />
             </div>
-            <small className="hidden text-left bg-soft_Red pl-2 py-2 italic rounded">
-              Whoops, make sure it’s an email
-            </small>
+            {!isValid ? (
+              <small className="block text-left bg-soft_Red pl-2 py-2 italic rounded">
+                Whoops, make sure it’s an email
+              </small>
+            ) : null}
           </div>
           <Button
             className="leading-7 md:w-auto  hover:bg-white hover:bg-soft_Red tracking-[0.25px] mt-4 md:mt-0 w-full"
