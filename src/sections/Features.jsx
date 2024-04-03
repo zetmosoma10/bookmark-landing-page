224dea2cb5fd6709be5952009be16ea03e375ec6
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-04-03 20:50:53 +0200

    add dynamic styles on active feature buttons

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -3,8 +3,8 @@ import Button from "../components/Button";
 import { featuresData } from "../constant";
 import featureBgIcon from "../Assets/images/feature-ractangle.svg";
 const Features = () => {
-  const [feature, setFeature] = useState(0);
-  console.log(featuresData[feature].id);
+  const [featuresDataArray, setFeaturesDataArray] = useState(featuresData);
+  const [featureIndex, setFeatureIndex] = useState(0);
   return (
     <section className="my-10 flex flex-col items-center max-container text-dark_Blue">
       <header className="text-center max-w-[540px]">
@@ -21,13 +21,21 @@ const Features = () => {
         className="w-full flex flex-col md:flex-row md:justify-center mt-10 md:mt-[72px] divide-y divide-dark_Blue 
         divide-opacity-20 md:divide-none border-b "
       >
-        {featuresData.map((feature, index) => (
+        {featuresDataArray.map((feature, index) => (
           <Button
-            onClick={() => setFeature(feature.id)}
+            onClick={() => setFeatureIndex(feature.id)}
+            style={{
+              borderBottom:
+                featuresDataArray[featureIndex].id === index
+                  ? "1px solid #FA5959"
+                  : null,
+              opacity:
+                featuresDataArray[featureIndex].id === index ? "1" : "0.5",
+            }}
             key={index}
             type="button"
             className="font-normal opacity-75  shadow-none rounded-none active:scale-100
-              first:border-t first:md:border-t-0"
+              first:border-t first:md:border-t-0 "
           >
             {feature.button}
           </Button>
@@ -37,7 +45,7 @@ const Features = () => {
         <div className="relative md:justify-self-end md:mr-10">
           <img
             className="w-full md:w-auto md:max-w-[536px]"
-            src={featuresData[feature].imgUrl}
+            src={featuresDataArray[featureIndex].imgUrl}
             alt=""
           />
           <img
@@ -48,13 +56,13 @@ const Features = () => {
         </div>
         <div className="text-center md:text-left">
           <h3 className="font-medium text-2xl md:text-3xl leading-[52px] tracking-[-0.08px] md:tracking-[-0.1px]  ">
-            {featuresData[feature].title}
+            {featuresDataArray[featureIndex].title}
           </h3>
           <p className="text-base md:text-lg md:leading-[28px] md:mt-4  opacity-50">
-            {featuresData[feature].text}
+            {featuresData[featureIndex].text}
           </p>
           <Button className="mt-4" type="link" variant="blue">
-            {featuresData[feature].link}
+            {featuresDataArray[featureIndex].link}
           </Button>
         </div>
       </div>
